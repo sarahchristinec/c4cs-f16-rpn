ed368623397e94e91dc50109410bae652b9f8618
sarahcc <sarahcc@umich.edu>
2016-10-09 14:42:54 -0400

    Implemented exponentiation

--- rpn.py
+++ rpn.py
@@ -3,34 +3,32 @@
 import operator
 
 operators = {
-        '+':operator.add,
-        '-':operator.sub,
-        '*':operator.mul,
-        '/':operator.truediv,
-
+    '+':operator.add,
+    '-':operator.sub,
+    '*':operator.mul,
+    '/':operator.truediv,
+    '^':operator.pow,
 }
 
 def calculate(string):
-        stack = list()
-        for token in string.split():
-                try:
-                        stack.append(int(token))
-                except ValueError:
-                        arg2 = stack.pop()
-                        arg1 = stack.pop()
-                        function = operators[token]
-                        result = function(arg1, arg2)
-                        stack.append(result)
-                print(stack)
-        if len(stack) != 1:
-                raise TypeError
-        return stack.pop()
+    stack = list()
+    for token in string.split():
+        try:
+            stack.append(int(token))
+        except ValueError:
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            function = operators[token]
+            result = function(arg1, arg2)
+            stack.append(result)
+        print(stack)
+    if len(stack) != 1:
+        raise TypeError
+    return stack.pop()
 
 def main():
-        while True:
-                calculate(input("rpn calc>"))
+    while True:
+        calculate(input("rpn calc>"))
 
 if __name__ == '__main__':
-        main()
-
-
+    main()
