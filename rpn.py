5b871dbe6614b0797f8af69021641dad365839cc
sarahcc <sarahcc@umich.edu>
2016-10-09 13:38:36 -0400

    final using operators library

--- rpn.py
+++ rpn.py
@@ -1,39 +1,36 @@
 #!/usr/bin/env python3
 
+import operator
+
+operators = {
+        '+':operator.add,
+        '-':operator.sub,
+        '*':operator.mul,
+        '/':operator.truediv,
+
+}
+
 def calculate(string):
-	pass
-	stack = list()
-	for token in string.split():
-		if token == '+':
-			arg1 = stack.pop()
-			arg2 = stack.pop()
-			result = arg1 + arg2
-			stack.append(result)
-		elif token == '-':
-			arg1 = stack.pop()
-			arg2 = stack.pop()
-			result = arg2 - arg1
-			stack.append(result)
-		elif token == '*':
-			arg1 = stack.pop()
-			arg2 = stack.pop()
-			result = arg2 * arg1
-			stack.append(result)
-		elif token == '/':
-			arg1 = stack.pop()
-			arg2 = stack.pop()
-			result = arg2 / arg1
-			stack.append(result)
-		else:
-			stack.append(int(token))
-		print(stack)
-	if len(stack) != 1:
-		raise TypeError
-	return stack.pop()
+        stack = list()
+        for token in string.split():
+                try:
+                        stack.append(int(token))
+                except ValueError:
+                        arg2 = stack.pop()
+                        arg1 = stack.pop()
+                        function = operators[token]
+                        result = function(arg1, arg2)
+                        stack.append(result)
+                print(stack)
+        if len(stack) != 1:
+                raise TypeError
+        return stack.pop()
 
 def main():
-	while True:
-		calculate(input("rpn calc>"))
+        while True:
+                calculate(input("rpn calc>"))
 
 if __name__ == '__main__':
-	main()
+        main()
+
+
